70519b9c85dca378c2e686c481059542400b73ed
Abdurrahman Abdul Hamid <74585156+Artcods@users.noreply.github.com>
2021-05-21 19:24:15 +0700

    Membuat Konsep Dasar Python 1

---
+++ Konsep-Python-1.py
@@ -0,0 +1,102 @@
+# Konsep-Python 1
+
+print("Compile") # Ini Tentang Compile  
+
+
+print("Hello") 
+
+print("World") 
+
+print("Hello World") 
+
+# Tanda Comment (#) 
+  
+"""Stay Save Brooo MultiLine""" 
+
+a = 10 # Ini tanda juga
+
+print(a) 
+
+
+# Cara Mengcompile, Buka Terminal dan Tuliskan 
+# python -m py_compile "YOURFILE.py"
+
+
+print("Variabel")
+# Variabel adalah tempat penyimpan data
+# dan menaruh Assignment nilai
+
+a = 10
+b = 5
+panjang = 25
+
+# Panggilan Pertama
+
+print("Nilai a =", a)
+print("Nilai b =", b)
+print("Nilai panjang =", panjang)
+# pada ("Nilai a =") adalah compile dan (a) variabel
+
+
+# Panggilan Assignment Indirect
+
+x = a 
+
+print("Nilai = b", a)
+
+
+
+
+print("tipe data") 
+
+
+# Tipe date : Angka satuan (Integer) 
+# ket : Angka Integer tidak berkoma  
+
+data_integer = 11 # Ini bertipe Integer 
+print(type(data_integer)) # Tanda Aje 
+
+print("data:", data_integer) # Int 
+
+print("- bertipe :", type(data_integer)) # Int 
+
+ 
+
+# Tipe data : Angka Dengan Koma (Float) 
+
+data_flote = 1.5 
+print("data:", data_flote) # float 
+
+print("- bertipe :", type(data_flote)) # float 
+
+ 
+
+# Tipe data : Kumpulan Karakter (String) 
+
+data_string = "mantap" # Yang biasa digunakan 
+print("data:", data_string) # str 
+
+print("- bertipe :", type(data_string)) # str 
+
+ 
+
+# Tipe data : Biner true/false (Boolean) 
+
+data_bool = True # Nilai nya bisa true/false 
+print("data:", data_bool) # bool 
+
+print("- bertipe :", type(data_bool)) # bool 
+
+ 
+
+# Tipe data khusus  
+
+# Bilang Kompleks  
+
+data_complex = complex(5,6) 
+
+print("data:", data_complex) # Complex 
+
+print("- bertipe :", type(data_complex)) # Complex 
+
+

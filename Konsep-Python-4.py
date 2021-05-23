872649088eff308ab13733e4b8bd162c6a2a240e
Abdurrahman Abdul Hamid <74585156+Artcods@users.noreply.github.com>
2021-05-23 09:27:49 +0700

    Create Konsep-Python-4.py

---
+++ Konsep-Python-4.py
@@ -0,0 +1,75 @@
+# Konsep Python -4 
+
+ 
+  
+print("=====>Konversi Satuan Temperature<=====") 
+# Program Konversi Celsius ke Satuan yg lain 
+
+ 
+  
+print("\n===PROGRAM KONVERSI TEMPERATURE===\n") 
+ 
+
+celcius = float(input("Masukkan suhu : ")) 
+
+print("Suhu Celsius adalah", celcius, "Celcius") 
+ 
+
+print("--->Reamur") 
+ 
+
+reamur = (4/5) * celcius 
+print("Suhu Reamur adalah", reamur, "Reamur") 
+ 
+
+print("--->Fahrenheit")
+ 
+
+fahrenheit = ((9/5) * celcius) + 32 
+print("Suhu Fahrenheit adalah",fahrenheit, "fahrenheit") 
+
+
+print("--->kelvin") 
+ 
+
+kelvin = celcius + 273 
+print("Suhu Kelvin", kelvin, "Kelvin") 
+
+ 
+  
+ print("========================================================================")
+
+
+
+# mencari suhu fahrenheit ke kelvin dan, kelvin ke fahrenheit
+print("=====>Fahrenhait Ke Kelvin<=====") 
+
+ 
+
+fahrenheit = float(input("Masukkan Suhu : ")) 
+
+print("Suhu Fahrenheit adalah", fahrenheit,("Fahrenheit")) 
+
+
+celcius = fahrenheit - 32 # rumus
+kelvin = ((5/9) * celcius) + 273 # rumus
+
+
+print("Hasil Suhu Kelvin ",kelvin, "Kelvin") 
+
+ 
+
+print("=====>Kelvin Ke Fahrenheit<=====") 
+
+ 
+
+kelvin = float(input("Masukkan suhu : ")) 
+
+print("Suhu Kelvin : ", kelvin, ("Kelvin")) 
+
+
+celcius = kelvin - 273 # rumus
+fahrenheit = ((9/5) * celcius) + 32 # rumus
+
+ 
+print("Hasil Suhu Kelvin ke Fahrenheit adalah ",fahrenheit, "Fahrenheit") 
